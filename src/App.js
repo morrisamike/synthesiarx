566daf8c06b6381dfc92560b2efd86e42c0c60cb
Michael <morrisamike@gmail.com>
2022-10-26 08:51:52 -0400

    Initial commit - added ExpenseItems

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visisble!!!!!</p>
+      <ExpenseItem></ExpenseItem>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+
+function ExpenseItem (){
+
+    return(
+        <h2>ExpenseItem in here!
+            <p>added a simple function component</p>
+        </h2>
+
+    ) 
+}
+
+export default ExpenseItem;
