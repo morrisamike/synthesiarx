c65e238b435626cb3057ecbeac422ccb42b3796c
Michael <morrisamike@gmail.com>
2022-10-26 11:33:43 -0400

    added place holders

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
 
+import './ExpenseItem.css';
 
-function ExpenseItem (){
-
+function ExpenseItem() {
+    const d = new Date();
+    const expenseDate = new Date(d.getFullYear().toString() ,d.getMonth().toString() ,d.getDate().toString());
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 264.95;
     return(
-        <h2>ExpenseItem in here!
-            <p>added a simple function component</p>
-        </h2>
-
-    ) 
+        <div className='expense-item'>
+            <div>{'Todays Date:' +  expenseDate.toDateString()} </div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2> 
+                <div className='expense-item__price'>${expenseAmount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
